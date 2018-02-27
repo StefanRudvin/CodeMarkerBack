4dcf1cda011ce1497fc47bcbcec693597dc7d3b5
StefanRudvin <stefan.rudvin@hotmail.com>
2018-02-27 17:58:25 +0000

    Allow new user creation

--- app/serializers.py
+++ app/serializers.py
@@ -79,7 +79,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create(
-            username=validated_data['username']
+            username=validated_data['username'], email=validated_data['email']
         )
         user.set_password(validated_data['password'])
         user.save()
